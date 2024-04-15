49c10487919aed37f9640571d8a3b952c96a5e29
tral909 <indorm1992@mail.ru>
2024-04-15 09:52:12 +0300

    Add liquibase tables

--- src/main/java/com/example/cqrsbankingapp/repository/EventRepository.java
+++ src/main/java/com/example/cqrsbankingapp/repository/EventRepository.java
@@ -1,7 +1,7 @@
 package com.example.cqrsbankingapp.repository;
 
-import com.example.cqrsbankingapp.event.Event;
+import com.example.cqrsbankingapp.event.AbstractEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<AbstractEvent, Long> {
 }
